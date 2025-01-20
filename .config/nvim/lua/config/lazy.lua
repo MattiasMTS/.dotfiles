5b8672cbfe9062ceeedc92477b2bdbfaa1643200
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-01-20 16:09:28 +0100

    chore: auto-sync dotfiles

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -49,6 +49,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.nix" },
+    { import = "lazyvim.plugins.extras.dap.core" },
 
     -- AI stuff
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -23,6 +23,94 @@ in
         # wvous-bl-corner = 3;  # bottom-left - Application Windows
         # wvous-br-corner = 4;  # bottom-right - Desktop
       };
+
+      # customize trackpad
+      trackpad = {
+        Clicking = true;  # enable tap to click
+        TrackpadRightClick = true;  # enable two finger right click
+        TrackpadThreeFingerDrag = true;  # enable three finger drag
+      };
+
+      # customize settings that not supported by nix-darwin directly
+      # Incomplete list of macOS `defaults` commands :
+      #   https://github.com/yannbertrand/macos-defaults
+      NSGlobalDomain = {
+        "com.apple.swipescrolldirection" = false;  # enable natural scrolling(default to true)
+        "com.apple.sound.beep.feedback" = 0;  # disable beep sound when pressing volume up/down key
+        AppleInterfaceStyle = "Dark";  # dark mode
+        AppleKeyboardUIMode = 3;  # Mode 3 enables full keyboard control.
+        ApplePressAndHoldEnabled = true;  # enable press and hold
+
+        # sets how long it takes before it starts repeating.
+        InitialKeyRepeat = 15;  # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+        # sets how fast it repeats once it starts.
+        KeyRepeat = 3;  # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+      };
+
+      CustomUserPreferences = {
+        ".GlobalPreferences" = {
+          # automatically switch to a new space when switching to the application
+          AppleSpacesSwitchOnActivate = true;
+        };
+        NSGlobalDomain = {
+          # Add a context menu item for showing the Web Inspector in web views
+          WebKitDeveloperExtras = true;
+        };
+        # "com.apple.finder" = {
+        #   ShowExternalHardDrivesOnDesktop = true;
+        #   ShowHardDrivesOnDesktop = true;
+        #   ShowMountedServersOnDesktop = true;
+        #   ShowRemovableMediaOnDesktop = true;
+        #   _FXSortFoldersFirst = true;
+        #   # When performing a search, search the current folder by default
+        #   FXDefaultSearchScope = "SCcf";
+        # };
+        "com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.spaces" = {
+          "spans-displays" = 0; # Display have seperate spaces
+        };
+        "com.apple.WindowManager" = {
+          EnableStandardClickToShowDesktop = 0; # Click wallpaper to reveal desktop
+          StandardHideDesktopIcons = 0; # Show items on desktop
+          HideDesktop = 0; # Do not hide items on desktop & stage manager
+          StageManagerHideWidgets = 0;
+          StandardHideWidgets = 0;
+        };
+        "com.apple.screensaver" = {
+          # Require password immediately after sleep or screen saver begins
+          askForPassword = 1;
+          askForPasswordDelay = 0;
+        };
+        # "com.apple.screencapture" = {
+        #   location = "~/Desktop";
+        #   type = "png";
+        # };
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
+        # Prevent Photos from opening automatically when devices are plugged in
+        "com.apple.ImageCapture".disableHotPlug = true;
+      };
+
+      loginwindow = {
+        GuestEnabled = false;  # disable guest user
+        SHOWFULLNAME = true;  # show full name in login window
+      };
+    };
+
+    # keyboard settings
+    keyboard = {
+      enableKeyMapping = true;
+
+      # NOTE: do NOT support remap capslock to both control and escape at the same time
+      remapCapsLockToControl = true;
+      remapCapsLockToEscape  = false;
     };
   };
+
+  security.pam.enableSudoTouchIdAuth = true;
 }
