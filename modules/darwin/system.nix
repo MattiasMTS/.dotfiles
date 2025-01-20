54b5f5f07b12b6c23f6e26beb4e7a997bddfdd29
MattiasMTS <mattias.sjodin.6764@hotmail.com>
2025-01-20 17:20:36 +0100

    chore: auto-sync dotfiles

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -9,6 +9,12 @@ in
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
     stateVersion = 5;
+    # activationScripts are executed every time you boot the system or run `darwin-rebuild`.
+    activationScripts.postUserActivation.text = ''
+      # activateSettings -u will reload the settings from the database and apply them to the current session,
+      # so we do not need to logout and login again to make the changes take effect.
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
 
     # Customize the system configuration
     defaults = {
